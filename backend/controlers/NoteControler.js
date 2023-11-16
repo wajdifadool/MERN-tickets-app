2ec3f517a9931c61cbca391a2b4c5bb45584fe52
wajdifadool <fadoolwajdi@gmail.com>
2023-11-16 12:40:45 +0200

    add Anote

--- backend/controlers/NoteControler.js
+++ backend/controlers/NoteControler.js
@@ -37,6 +37,42 @@ const getNotes = asyncHandler(async (req, res) => {
   res.status(200).json(notes);
 });
 
+/**
+ *  @desc Create notes for a ticket
+ *  @route POST /api/tickets/:ticketId/notes
+ *  @access Private
+ */
+const addNote = asyncHandler(async (req, res) => {
+  // the protect function have the user in the req argumnet
+  const user = req.user;
+  if (!user) {
+    res.status(401);
+    throw new Error('NoteControler.js::getNotes(), User not found ');
+  }
+
+  // Grab the ticket
+  const ticket = await Ticket.findById(req.params.ticketId); // the id from the url
+
+  // make sure its the user ticket
+  // each ticket have the user id in filed called user
+  // the request for the cuurent user
+  if (ticket.user.toString() !== req.user.id) {
+    // not Auth
+    res.status(401);
+    throw new Error('User not authorized');
+  }
+
+  const note = await Note.create({
+    ticket: req.params.ticketId,
+    text: req.body.text, // comes from form
+    isStaff: false,
+    user: req.user.id,
+  });
+
+  res.status(200).json(note);
+});
+
 module.exports = {
   getNotes,
+  addNote,
 };
--- backend/routes/NoteRoute.js
+++ backend/routes/NoteRoute.js
@@ -3,9 +3,10 @@ const router = express.Router({ mergeParams: true });
 
 // evrey time we need to protect route , we pass the protect cosnt as 2nd arg
 const { protect } = require('../middleware/authMiddleware');
-const { getNotes } = require('../controlers/NoteControler');
+const { getNotes, addNote } = require('../controlers/NoteControler');
 
 // route format /api/tickets/:ticketId/notes
 router.route('/').get(protect, getNotes);
+router.route('/').post(protect, addNote);
 
 module.exports = router;
